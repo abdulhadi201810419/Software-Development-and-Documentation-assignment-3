57f2ae2beef4899109f0384d03d4ddcd7525a8bd
abdulhadi201810419 <122031052+abdulhadi201810419@users.noreply.github.com>
2023-01-05 17:34:24 +0300

    Add files via upload

---
+++ assignment 3/assignment 3 part 2/java1.java
@@ -0,0 +1,22 @@
+public class BankAccount{
+
+    String owner;
+    Dollars balance= 0;
+
+    public void deposit(Dollars amount){
+       balance= balance + amount;
+    }
+
+    public withdrawal(Dollars amount){
+       balance= balance - amount;
+
+    }
+
+
+}
+
+
+
+
+
+
